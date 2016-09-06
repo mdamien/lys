c62e53ef53aceff30b3682aeadcab1a045818cdd
Damien <damien@dam.io>
2016-09-06 04:00:27 +0200

    0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='lys',
-    version='0.1',
+    version='0.2',
     description='Simple HTML templating for Python',
     long_description=open('README.rst').read(),
     url='http://github.com/mdamien/lys',
