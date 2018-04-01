6b0b3c7e0bcbd0841e914dc033a79cf4d61b117f
Damien <damien@dam.io>
2018-04-01 10:30:04 +0200

    0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='lys',
-    version='0.2',
+    version='0.3',
     description='Simple HTML templating for Python',
     long_description=open('README.rst').read(),
     url='http://github.com/mdamien/lys',
@@ -14,14 +14,14 @@ setup(
     tests_require=['nose'],
     license='MIT',
     install_requires=["future"] if sys.version_info < (3,) else [],
-    classifiers=(
+    classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-    ),
+    ],
     packages=['lys'],
     zip_safe=False
 )
